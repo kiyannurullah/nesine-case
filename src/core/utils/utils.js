6767b5c21b0cd814407a2372f154f587254d1483
NURULLAH KIYAN <nurullah.kiyan@atmosware.turkcell.com.tr>
2023-03-20 02:24:51 +0300

    undefined keys changed and favicon added

--- src/core/utils/utils.js
+++ src/core/utils/utils.js
@@ -1,7 +1,7 @@
 export const eventDrawer = (index, item) => `${index} ${item?.D} ${item?.DAY} ${item?.LN}`;
 
 export const eventData = (odd, eIndex, eId, eName, i) => {
-  odd.index = `${eIndex}-${i}-${odd?.ID}`;
+  odd.index = `${eIndex}-${i}-${odd?.ID ||`${i}-null` }`;
   odd.eName = eName;
   odd.eId = eId;
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,7 @@ function App({ Component, pageProps }) {
           name="viewport"
           content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width"
         />
+        <link rel="icon" type="image/png" href="/../assets/img/favicon-32x32.png" sizes="32x32" />
         <title>Nesine Case</title>
       </Head>
       <CouponContext>
