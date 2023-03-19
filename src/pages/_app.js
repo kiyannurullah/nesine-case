e5cf0e2d8d7eb715a229512b6082c927530d5e7e
NURULLAH KIYAN <nurullah.kiyan@atmosware.turkcell.com.tr>
2023-03-20 02:27:23 +0300

    favicon move to public folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function App({ Component, pageProps }) {
           name="viewport"
           content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width"
         />
-        <link rel="icon" type="image/png" href="/../assets/img/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         <title>Nesine Case</title>
       </Head>
       <CouponContext>
